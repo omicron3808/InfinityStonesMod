a0bc9b2013793732367b965a89fd117f8abb61fd
omicron3808 <omicron3808@gmail.com>
2019-04-21 20:34:12 -0700

    Initial commit

---
+++ java/OmiDelZ_StSMod/InfinityStonesMod.java
@@ -0,0 +1,18 @@
+package OmiDelZ_StSMod;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import basemod.BaseMod;
+import OmiDelZ_StSMod.custom_relics.*;
+
+@SpireInitializer
+public class InfinityStonesMod {
+
+    public InfinityStonesMod() {
+
+    }
+
+    public static void initialize() {
+        new InfinityStonesMod();
+    }
+
+}
---
+++ java/OmiDelZ_StSMod/custom_relics/RealityStone.java
@@ -0,0 +1,4 @@
+package OmiDelZ_StSMod.custom_relics;
+
+public class RealityStone {
+}
